3e154d0b1a1108247a2589a51e66fd3a91880396
ptr <piotr2785.owca@gmail.com>
2026-02-26 21:25:53 +0100

    Lab3.8

---
+++ Lab 3/Lab03_APP_NET_SQLite/Lab03_APP_NET_SQLite/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using Lab03_APP_NET_SQLite;
+using Microsoft.EntityFrameworkCore;
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab03_APP_NET_SQLite.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; } = new();
+    }
+}
